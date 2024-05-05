7df81d00cc9da292f01405e343a65b2aa5d3d8dd
Andreas Huber <andreas@jolhof.de>
2024-05-05 23:12:50 +0200

    fix here maps

--- src/components/map.js
+++ src/components/map.js
@@ -5,7 +5,7 @@ import { feuerwehrRot } from "./layout";
 import fwhaus from "../images/fwhaus_icon.svg";
 import Points from "../data/points";
 
-const coreUrl = "https://js.api.here.com/v3/3.0/mapsjs-core.js";
+const coreUrl = "https://js.api.here.com/v3/3.1/mapsjs-core.js";
 
 const appId = process.env.APP_ID || process.env.GATSBY_APP_ID;
 const appCode = process.env.APP_CODE || process.env.GATSBY_APP_CODE;
@@ -15,9 +15,10 @@ if (!appId || !appCode) {
 }
 
 const urls = [
-  "https://js.api.here.com/v3/3.0/mapsjs-service.js",
-  "https://js.api.here.com/v3/3.0/mapsjs-ui.js",
-  "https://js.api.here.com/v3/3.0/mapsjs-mapevents.js",
+
+  "https://js.api.here.com/v3/3.1/mapsjs-service.js",
+  "https://js.api.here.com/v3/3.1/mapsjs-ui.js",
+  "https://js.api.here.com/v3/3.1/mapsjs-mapevents.js",
 ];
 
 // data
@@ -42,9 +43,7 @@ class Map extends React.Component {
   drawMap() {
     if (!this.platform) {
       this.platform = new window.H.service.Platform({
-        app_id: appId,
-        app_code: appCode,
-        useHTTPS: true,
+        apikey: appCode,
       });
       const pixelRatio = window.devicePixelRatio || 1;
       const defaultLayers = this.platform.createDefaultLayers({
@@ -54,14 +53,14 @@ class Map extends React.Component {
 
       this.map = new this.H.Map(
         document.getElementById("map"),
-        defaultLayers.normal.map,
+        defaultLayers.raster.satellite.map,
         {
           pixelRatio: pixelRatio,
           center: fwHausCoords,
           zoom: 13,
         }
       );
-      this.map.setBaseLayer(defaultLayers.satellite.map);
+      this.map.setBaseLayer(defaultLayers.raster.satellite.map);
 
       //const behavior = new this.H.mapevents.Behavior(new this.H.mapevents.MapEvents(this.map));
 
@@ -73,21 +72,28 @@ class Map extends React.Component {
     if (this.props.lat && this.props.lng) {
       this.calculateRouteFromAtoB();
     } else {
-      this.addFWHausIcon();
-      this.addEinsatzbereich();
+      const fwHausIcon = new this.H.map.Icon(fwhaus, { anchor: { x: 25, y: 23.6 } });
+      const fwHausMarker = new this.H.map.Marker(
+        fwHausCoords,
+        { icon: fwHausIcon }
+      );
+      const einsatzbereich = this.addEinsatzbereich();
+      const group = new this.H.map.Group();
+      group.addObjects([einsatzbereich, fwHausMarker]);
+      // Add the group to the map
+      this.map.addObject(group);
       new this.H.mapevents.Behavior(new this.H.mapevents.MapEvents(this.map));
     }
   }
   calculateRouteFromAtoB() {
-    const router = this.platform.getRoutingService(),
-      routeRequestParams = {
-        mode: "fastest;car",
-        representation: "display",
-        routeattributes: "waypoints,summary,shape,legs",
-        maneuverattributes: "direction,action",
-        waypoint0: `${fwHausLat},${fwHausLng}`, // Feuerwehrhaus
-        waypoint1: `${this.props.lat},${this.props.lng}`, // Ziel
-      };
+    const router = this.platform.getRoutingService(null, 8);
+    const routeRequestParams = {
+      routingMode: "fast",
+      transportMode: 'car',
+      origin: `${fwHausLat},${fwHausLng}`, // Feuerwehrhaus
+      destination: `${this.props.lat},${this.props.lng}`,
+      return: 'polyline',
+    };
 
     router.calculateRoute(
       routeRequestParams,
@@ -96,60 +102,58 @@ class Map extends React.Component {
     );
   }
   onSuccess(result) {
-    const route = result.response.route[0];
-    this.addRouteShapeToMap(route);
-    this.addSummaryToPanel(route.summary);
-    this.addFWHausIcon();
-    this.addEndPoint({ lat: this.props.lat, lng: this.props.lng });
+    if (result.routes.length < 1) {
+      console.warn('routing was not successful');
+      return;
+    }
+    // const lineStrings = [];
+    const route = result.routes[0];
+    const lineStrings = [];
+    route.sections.forEach((section) => {
+      lineStrings.push(this.H.geo.LineString.fromFlexiblePolyline(section.polyline));
+    })
+    const multiLineString = new this.H.geo.MultiLineString(lineStrings);
+
+    const routeLine = new this.H.map.Polyline(multiLineString, {
+      style: {
+        strokeColor: "rgba(168, 28, 28, 0.7)",
+        lineWidth: 4
+      }
+    });
+    const fwHausIcon = new this.H.map.Icon(fwhaus, { anchor: { x: 25, y: 23.6 } });
+    const startMarker = new this.H.map.Marker(
+      fwHausCoords,
+      { icon: fwHausIcon }
+    );
+    const svgMarkup =
+      '<svg width="18" height="18" ' +
+      'xmlns="http://www.w3.org/2000/svg">' +
+      '<circle cx="8" cy="8" r="8" ' +
+      `fill="${feuerwehrRot}" stroke="white" stroke-width="1"  />` +
+      "</svg>",
+      dotIcon = new this.H.map.Icon(svgMarkup, { anchor: { x: 8, y: 8 } });
+    const endMarker = new this.H.map.Marker({ lat: this.props.lat, lng: this.props.lng }, { icon: dotIcon });
+
+    const group = new this.H.map.Group();
+    group.addObjects([routeLine, startMarker, endMarker]);
+    // Add the group to the map
+    this.map.addObject(group);
+
+    // Set the map viewport to make the entire route visible:
+    let bounds = group.getBoundingBox();
+    bounds.x -= 0.002;
+    bounds.da += 0.002;
+    bounds.ha -= 0.002;
+    bounds.ga += 0.002;
+    this.map.getViewModel().setLookAtData({
+      bounds: bounds
+    });
+
   }
   onError(error) {
     console.error("error while routing", error);
   }
-  addRouteShapeToMap(route) {
-    const strip = new this.H.geo.Strip(),
-      routeShape = route.shape;
 
-    routeShape.forEach(function (point) {
-      var parts = point.split(",");
-      strip.pushLatLngAlt(parts[0], parts[1]);
-    });
-
-    const polyline = new this.H.map.Polyline(strip, {
-      style: {
-        lineWidth: 4,
-        strokeColor: "rgba(168, 28, 28, 0.7)",
-      },
-    });
-    // Add the polyline to the map
-    this.map.addObject(polyline);
-    this.mapObjects.push(polyline);
-    // And zoom to its bounding rectangle
-    this.map.setViewBounds(polyline.getBounds(), true);
-  }
-  addFWHausIcon() {
-    const icon = new this.H.map.Icon(fwhaus, { anchor: { x: 25, y: 23.6 } });
-    const marker = new this.H.map.Marker(
-      fwHausCoords,
-      { icon: icon }
-    );
-    this.map.addObject(marker);
-  }
-  addEndPoint(point) {
-    const svgMarkup =
-        '<svg width="18" height="18" ' +
-        'xmlns="http://www.w3.org/2000/svg">' +
-        '<circle cx="8" cy="8" r="8" ' +
-        `fill="${feuerwehrRot}" stroke="white" stroke-width="1"  />` +
-        "</svg>",
-      dotIcon = new this.H.map.Icon(svgMarkup, { anchor: { x: 8, y: 8 } });
-    const marker = new this.H.map.Marker(point, { icon: dotIcon });
-    this.map.addObject(marker);
-    this.mapObjects.push(marker);
-  }
-  addSummaryToPanel(summary) {
-    const text = `Anfahrtsweg: ${summary.distance / 1000}km`;
-    this.setState({ summary: text });
-  }
   addEinsatzbereich() {
     const lineString = new this.H.geo.LineString(Points, "values lat lng alt");
     const polygon = new this.H.map.Polygon(lineString, {
@@ -159,8 +163,7 @@ class Map extends React.Component {
         lineWidth: 0,
       },
     });
-    this.map.addObject(polygon);
-    this.map.setViewBounds(polygon.getBounds(), true);
+    return polygon;
   }
   componentDidMount() {
     if (!window.H || !window.H.service) {
@@ -185,7 +188,7 @@ class Map extends React.Component {
       this.drawMap();
     }
   }
-  componentWillUnmount() {}
+  componentWillUnmount() { }
   render() {
     return (
       <div>
